4d212d8e3e600b8b034c62171ce18a8a1c78f2c2
costar011 <dnaer456@gmail.com>
2022-11-19 23:45:15 +0900

    row

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className='header'></div>
+      <div className="header">
+        <div className="logo">logo</div>
+        <div className="menu">menu</div>
+      </div>
     </div>
   );
 }
